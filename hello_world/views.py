c2bba6137865e787ae9990409af1847252346a12
luk8982 <luk8982@users.noreplay.github.com>
2018-04-21 13:25:48 +0200

    zmiana imienia LXkasz

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Luuukasz"
+moje_imie = "LXkasz"
 msg = "Hello World!"
 
 
--- test/test_views.py
+++ test/test_views.py
@@ -14,4 +14,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"Luuukasz", "mgs":"Hello World!"}', rv.data)
+        self.assertEquals('{ "imie":"LXkasz", "mgs":"Hello World!"}', rv.data)
